12e3e6b8f15168fd0559c3f6d7143783689aa8c6
Li Zhang <sumeiya@Lis-MacBook-Pro.local>
2020-10-18 10:52:40 -0700

    midterm

--- zhang.li/js/app.js
+++ zhang.li/js/app.js
@@ -56,3 +56,55 @@ $(()=>{
    })
 
 })
+
+
+
+
+
+// Image Picker
+
+// This function just works and can be used for many file types.
+// It will accept multiple files, and will only fire the callback once for each file.
+// Don't try to reinvent this
+function readFiles(files,callback,index=0) {
+  if (files && files[0]) {
+    let file = files[index++],
+        reader = new FileReader();
+    reader.onload = function(e){
+      callback(e);
+      if(index<files.length) readFiles(files,callback,index);
+    }
+    reader.readAsDataURL(file);
+  }
+}
+
+
+
+
+
+
+// Create a selector for an input and then do whatever you want using the callback function.
+$("body")
+  .on("change",".imagepicker-replace input",function() {
+  // store the current "this" into a variable
+  var imagepicker = this;
+  readFiles(this.files,function(e) {
+    // "this" will be different in the callback function
+    $(imagepicker).parent()
+      .addClass("picked")
+      .css({"background-image":"url("+e.target.result+")"});
+  });
+})
+
+
+
+// This example will add a new thumbnail each time
+$("body")
+  .on("change",".imagepicker-add input",function() {
+  var imagepicker = this;
+  readFiles(this.files,function(e) {
+    $(imagepicker).parent().before(
+      "<div class='thumbnail' style='background-image:url("+e.target.result+")'></div>"
+    )
+  });
+});
